e7d1ea8647b5c5de0088571cf76a3185846ad18c
Florian Herkommer <florian.herkommer@gmx.de>
2021-06-04 14:06:18 +0200

    First implementation

--- 4002-RAM/4002.h
+++ 4002-RAM/4002.h
@@ -14,5 +14,159 @@
 // Declaring namespaces
 using namespace std;
 
+class Intel4002 : public Intel4002Base {
+public:
+	enum {
+        RAM_CELLS_EACH_CHIP = 256,
+        MAX_NUMBER_OF_RAM_CHIPS = 4,
+		MAX_NUMBER_OF_BANKS = 8
+    };
+	/**
+	 * Konstruktor
+	 */
+	Intel4002() {
+	}
+	/**
+	 * Destruktor
+	 */
+	virtual ~Intel4002() {
+	}
+	/**
+	 * Setzt die internen Register zurück zurück
+	 */
+	virtual void reset();
+	/**
+	 * Ist die RAM Adresse vorhanden.
+	 * Falls nicht wird false zurückgegeben
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @return <c>true</c> wenn addressierbar, sonst <c>false</c>
+	 */
+	virtual bool isRAMAdrAccessable(const ERAMBank bank, const ERAMChip chip, const int address) const;
+	/**
+	 * Ein Nibble an der derzeit angeforderten Adresse
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @return Wert
+	 */
+	virtual uint4_t readRAMNibble(const ERAMBank bank, const ERAMChip chip, const int address) const;
+	/**
+	 * Ein Nibble an der derzeit angeforderten Adresse
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @param value Wert
+	 * @return Ob an dieser Stelle schreibbar war
+	 */
+	virtual bool writeRAMNibble(const ERAMBank bank, const ERAMChip chip, const int address, const uint4_t value) const;
+	/**
+	 * Ist die Status Adresse vorhanden.
+	 * Falls nicht wird false zurückgegeben
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @return <c>true</c> wenn addressierbar, sonst <c>false</c>
+	 */
+	virtual bool isStatusAdrAccessable(const ERAMBank bank, const ERAMChip chip, const int address) const;
+	/**
+	 * Ein Nibble an der derzeit angeforderten Adresse
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @return Wert
+	 */
+	virtual uint4_t readStatusNibble(const ERAMBank bank, const ERAMChip chip, const int address) const;
+	/**
+	 * Ein Nibble an der derzeit angeforderten Adresse
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param address Speicherzellenaddresse
+	 * @param value Wert
+	 * @return Ob an dieser Stelle schreibbar war
+	 */
+	virtual bool writeStatusNibble(const ERAMBank bank, const ERAMChip chip, const int address, const uint4_t value) const;
+	/**
+	 * Liest ein Nibble aus dem Ausgangspuffer
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @return Wert
+	 */
+	virtual uint4_t readFromPortBuffer(const ERAMBank bank, const ERAMChip chip) const;
+	
+	// Unsere besseren Funktionen
 
+	/**
+	 * Schreibt ein Nibble in den Ausgangspuffer
+	 * @param bank Bank
+	 * @param chip Chip
+	 * @param value Wert
+	 */
+	virtual void writePortBuffer(const ERAMBank bank, const ERAMChip chip, uint4_t value);
+	/**
+	 * Setzt die aktuelle Bank
+	 * @param bank Bank
+	 */
+	virtual void setCurrentBank(const ERAMBank bank);
+	/**
+	 * Gibt die aktuelle Bank zurück
+	 * @return currentBank
+	 */
+	virtual ERAMBank getCurrentBank() const;
+	/**
+	 * Setzt den aktuellen Chip
+	 * @param chip Chip
+	 */
+	virtual void setCurrentChip(const ERAMChip chip);
+	/**
+	 * Gibt den aktuellen Chip zurück
+	 * @return currentChip
+	 */
+	virtual ERAMBank getCurrentChip() const;
+	/**
+	 * Setzt die aktuelle Adresse
+	 * @param address Adresse
+	 */
+	virtual void setCurrentAddress(const int address);
+	/**
+	 * Gibt die aktuelle Adresse zurück
+	 * @return currentAddress
+	 */
+	virtual ERAMBank getCurrentAddress() const;
+
+private:
+	ERAMBank currentBank;
+	ERAMChip currentChip;
+	int currentAddress;
+	uint4_t ***RAM;
+	uint4_t ***RAMStatus;
+	uint4_t **RAMPort;
+
+// int ***arr3D = new int**[X];
+// for(int i =0; i<X; i++){
+//    arr3D[i] = new int*[Y];
+//    for(int j =0; j<Y; j++){
+//        arr3D[i][j] = new int[Z];
+//        for(int k = 0; k<Z;k++){
+//           arr3D[i][j][k] = 0;
+//        }
+//    }
+// }
+
+// arr = new int **[height]();
+//     for (i = 0; i < height; i++)
+//     {
+//         arr[i] = new int *[width]();
+//         for (j = 0; j < width; j++)
+//             arr[i][j] = new int [depth]();
+//     }
+// for (i = 0; i < rows; i++)
+// {
+//     for (j = 0; j < columns; j++)
+//         delete[] arr[i][j];
+//     delete[] arr[i];
+// }
+// delete[] arr;
+};
 #endif // _4002_h_
--- 4004/4004.h
+++ 4004/4004.h
@@ -4,9 +4,15 @@
 // Include local header files
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 	#include "..\inc\MCS4BaseClasses.h"
+	#include "..\4001-ROM\4001.h"
+	#include "..\4002-RAM\4002.h"
 #elif __unix__
 	#include "../inc/MCS4BaseClasses.h"
+	#include "../4001-ROM/4001.h"
+	#include "../4002-RAM/4002.h"
 #endif
+
+#include "4004_stack.h"
 
 // Include global header files
 #include <cstdint>
@@ -14,5 +20,108 @@
 // Declaring namespaces
 using namespace std;
 
+class Intel4004 : public Intel4004Base {
+public:
+	enum {
+        MAX_NUMBER_OF_REGISTERS = 16
+    };
+	/**
+	 * Konstruktor
+	 */
+	Intel4004();
+	/**
+	 * Destruktor
+	 */
+	virtual ~Intel4004();
+	/**
+	 * Setzt den Prozessor zurück
+	 */
+	virtual void reset();
+	/**
+	 * Gibt den Wert des Übertrags aus.
+	 * <br>Hinweis:</br> Siehe studienarbeit-4004.pdf S.19
+	 * @return Übertrag
+	 */
+	virtual bool getCarry() const;
+	/**
+	 * Gibt den Wert des Akkumulators aus.
+	 * <br>Hinweis:</br> Siehe studienarbeit-4004.pdf S.19
+	 * @return Akkumulator
+	 */
+	virtual uint4_t getAccumulator() const;
+	/**
+	 * Gibt die aktuelle Adresse der Ablaufsteuerung aus.
+	 * <br>Hinweis:</br> Siehe studienarbeit-4004.pdf S.19
+	 * @return Adresse der Ablaufsteuerung
+	 */
+	virtual UBankedAddress getPC() const;
+	/**
+	 * Dient zum auslesen eines Registers (Register 0..15).
+	 * <br>Hinweis:</br> Siehe studienarbeit-4004.pdf S.19 und MCS-4_Assembly_Language_Programming_Manual_Dec73.pdf S. 11
+	 * @param reg Registernummer
+	 * @return Nibble
+	 */
+	virtual uint4_t getRegister(const ERegister reg);
+	/**
+	 * Dient zum auslesen eines Registerspaars (Register R1R0 ... R15R14).
+	 * <br>Hinweis:</br> Siehe studienarbeit-4004.pdf S.19 und MCS-4_Assembly_Language_Programming_Manual_Dec73.pdf S. 11
+	 * @param reg Registerpaar Nummer
+	 * @return Byte
+	 */
+	virtual uint8_t getRegisterPair(const ERegister reg);
+	/**
+	 * Gibt den ROM Handle zurück.
+	 * @return ROM Handle
+	 */
+	virtual Intel4001Base* getPtrToROM();
+	/**
+	 * Gibt den RAM Handle zurück.
+	 * @return RAM Handle
+	 */
+	virtual Intel4002Base* getPtrToRAM();
+	/**
+	 * Gibt den Stack Handle zurück.
+	 * @return Stack Handle
+	 */
+	virtual Intel4004StackBase* getPtrToStack();
+	/**
+	 * Gibt die Anzahl der Ticks (Subzyklen) zurück die bisher gemessen wurden.
+	 * <br>Hinweis:</br> Siehe studienarbeit-4004.pdf S.6
+	 * @return Anzahl Ticks
+	 */
+	virtual uint64_t getTicks() const;
+	/**
+	 * Setzt die Ticks zurück (Subzyklen)
+	 */
+	virtual void resetTicks();
+	/** Gibt den Pin zurück (Hardwareschnittstelle).
+	 * <br>Hinweis:</br> Siehe studienarbeit-4004.pdf S.6
+	 * @return <c>true</c> wenn eingeschaltet, sonst <c>false</c>
+	 */
+	virtual bool getTestPin() const;
+	/**
+	 * Setzt den Pin (Hardwareschnittstelle)
+	 * <br>Hinweis:</br> Siehe studienarbeit-4004.pdf S.6
+	 * @param value <c>true</c> wenn eingeschaltet, sonst <c>false</c>
+	 */
+	virtual void setTestPin(const bool value);
+	/** Ruft die Programmablaufschnittstelle auf. Über diese Schnittstelle können die Befehle Step für Step abgearbeitet werden. */
+	virtual void nextCommand();
 
+private:
+	bool carryFlag;
+	bool testPin;
+	uint4_t accumulator;
+	uint64_t ticks;
+	UBankedAddress PC;
+	uint4_t *registers;
+	Intel4001 *ROM;
+	Intel4002 *RAM;
+	Intel4004Stack *stack;
+	
+	/** Intel4004 operation functions
+	 *	PC zuerst erhöhen, dann Funktion aufrufen!!
+	 * 	Bei two-word-commands liest nextCommand() beide Bytes ein und übergibt diese an die jeweilige Funtion
+	 */
+};
 #endif // _4004_h_
